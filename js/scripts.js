83039d46f9d247546ced6f39e702decef2be26a1
David <18404740+sdfg487381@users.noreply.github.com>
2025-05-24 13:10:10 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,19 @@ function toggleMenu() {
   nav.classList.toggle('show');
 }
 
-  function toggleForm(event) {
-    event.preventDefault();
-    document.getElementById('quote-form').style.display = 'flex';
-  }
+function toggleForm(event) {
+  event.preventDefault();
+  document.getElementById('quote-form').style.display = 'flex';
+}
 
-  function closeForm() {
-    document.getElementById('quote-form').style.display = 'none';
-  }
+function closeForm() {
+  document.getElementById('quote-form').style.display = 'none';
+}
 
-  function openFranchisePopup() {
-    document.getElementById("franchise-popup").style.display = "flex";
-  }
-  
-  function closeFranchisePopup() {
-    document.getElementById("franchise-popup").style.display = "none";
-  }
+function openFranchisePopup() {
+  document.getElementById("franchise-popup").style.display = "flex";
+}
+
+function closeFranchisePopup() {
+  document.getElementById("franchise-popup").style.display = "none";
+}
