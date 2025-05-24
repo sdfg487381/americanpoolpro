d1e80c29a42147ecd3280a411746206cd5afd6fb
David <18404740+sdfg487381@users.noreply.github.com>
2025-05-23 21:59:07 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@ function toggleMenu() {
   const nav = document.getElementById('site-nav');
   nav.classList.toggle('show');
 }
+
+  function toggleForm(event) {
+    event.preventDefault();
+    document.getElementById('quote-form').style.display = 'flex';
+  }
+
+  function closeForm() {
+    document.getElementById('quote-form').style.display = 'none';
+  }
